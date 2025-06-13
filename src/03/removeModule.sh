386637947a63580a483bd8f3170871f7372483cc
Fluid Oxygen <tosha.iljin55@gmail.com>
2025-06-13 17:41:13 +0700

    От греха подальше

--- src/03/removeModule.sh
+++ src/03/removeModule.sh
@@ -1,15 +1,14 @@
 # Добавить отслеживание ошибок во все 3 функции
 
 rmByLog() { 
-	$log="/home/flox/DO4_LinuxMonitoring_v2.0.ID_356280-1/src/02/record.log"
+	local log="/home/flox/DO4_LinuxMonitoring_v2.0.ID_356280-1/src/02/record.log"
 
-	$directories=$(cat ../02/record.log | awk -F'|' '$1 !~ /\.[^/]*$/ {print $1}')
-
-	while IFS; read -r dir; do
-		if [[ -e $dir ]]; then
-			$(rm -rf "$dir")
+	while IFS= read -r dir; do
+		dir=$(echo "$dir" | xargs)
+		if [[ -e "$dir" ]]; then
+				rm -rf "$dir"
 		fi
-	done < "$directories"
+	done < <(awk -F'|' '$1 !~ /\.[^/]*$/ {print $1}' "$log")
 
 	# Добавить очистку log file
 	# Мозги себе не трахать лишней логикой, при очистке по дате, маске, log file не трогать!!!
@@ -17,23 +16,74 @@ rmByLog() {
 }
 
 rmByDate() {
-	echo "Начало временного промежутка. Пример: \033[32m2025-06-09 11:49\033[0m"
-	read dateStart
-	echo "Конец временного промежутка. Пример: \033[32m2025-06-09 11:55\033[0m"
-	read dateEnd
+	echo -e "Начало временного промежутка. Пример: \033[32m2025-06-09 11:49\033[0m"
+	read -r dateStart
+	echo -e "Конец временного промежутка. Пример: \033[32m2025-06-09 11:55\033[0m"
+	read -r dateEnd
 
 	# Тут добавить функци для проверки формата даты
 
-	$dateStart="$dateStart:00"
-	$dateEnd="$dateEnd:59"
-	$(find / \( -type f -o -type d \) -newerct "$dateStart" ! -newerct "dateEnd" -delete)
+	dateStart="$dateStart:00"
+	dateEnd="$dateEnd:59"
+
+	echo "$dateStart"
+	echo "$dateEnd"
+	$(find / \( -name "bin" \
+							-o -name "bin64" \
+							-o -name "proc" \
+							-o -name "lib" \
+							-o -name "run" \
+							-o -name "var" \
+							-o -name "sys" \
+							-o -name "snap" \
+							-o -name "tmp" \
+							-o -name "root" \
+							-o -path "/etc/multipath" \) \
+									-prune -o -type d -newermt "$dateStart" ! -newermt "$dateEnd" -print -exec rm -rf {} +)\
+	$(find / \( -name "bin" \
+							-o -name "bin64" \
+							-o -name "proc" \
+							-o -name "lib" \
+							-o -name "run" \
+							-o -name "var" \
+							-o -name "sys" \
+							-o -name "snap" \
+							-o -name "tmp" \
+							-o -name "root" \
+							-o -path "/etc/multipath" \) \
+									-prune -o -type f -newecrt "$dateStart" ! -newecrt "$dateEnd" -print -exec rm -rf {} +)
+	
 }
 
 rmByMask() {
 	echo "Введите маску, по которой необходимо удалить файлы"
 	read mask
 
-	$(find / \( -type f -o -type d \) -name "$mask" -delete)
+
+	$(find / \( -name "bin" \
+							-o -name "bin64" \
+							-o -name "proc" \
+							-o -name "lib" \
+							-o -name "run" \
+							-o -name "var" \
+							-o -name "sys" \
+							-o -name "snap" \
+							-o -name "tmp" \
+							-o -name "root" \
+							-o -path "/etc/multipath" \) \
+									-prune -o -type d -name "$mask" -print -exec rm -rf {} +)
+	$(find / \( -name "bin" \
+							-o -name "bin64" \
+							-o -name "proc" \
+							-o -name "lib" \
+							-o -name "run" \
+							-o -name "var" \
+							-o -name "sys" \
+							-o -name "snap" \
+							-o -name "tmp" \
+							-o -name "root" \
+							-o -path "/etc/multipath" \) \
+									-prune -o -type f -name "$mask" -print -exec rm -rf {} +)
 }
 
 removeFileDir() {
@@ -41,13 +91,13 @@ removeFileDir() {
 
 	case $mode in 
 		1)
-			$(rmByLog)
+			rmByLog
 			;;
 		2)
-			$(rmByDate)
+			rmByDate
 			;;
 		3)
-			$(rmByMask)
+			rmByMask
 			;;
 	esac
 }
