396f7b8dbbdffda254c09eb26dd2db961ee2e0a4
Fluid Oxygen <tosha.iljin55@gmail.com>
2025-06-15 14:05:49 +0700

    Начало 04 и изменение git ignore

---
+++ src/04/main.sh
@@ -0,0 +1,9 @@
+source ./randomModule.sh
+source ./recordModule.sh
+
+countRecord=$(getRandomMax "100" "1000")
+
+for (i=0; i<"$countRecord"; i++) {
+
+}
+
---
+++ src/04/randomModule.sh
@@ -0,0 +1,78 @@
+ maxRand=32767
+ minRand=0
+
+getRandomMax() {
+  local value=0
+
+  if [[ $# -eq 2 ]]; then
+    
+    local min=$1
+    local max=$2
+
+    while [[ $value -lt $min ]]; do
+      $value=$(($maxRand%$max))
+    done
+    
+  else 
+  
+    local max=$1
+    $value=$(($maxRand%$max))
+    
+  fi
+  
+  $value=(($value+1))
+  echo "$value"
+
+}
+
+
+
+
+getCountFolders() {  
+  local countFolders=$(($RANDOM%10)) 
+  countFolders=$(($countFolders+1))
+  echo "$countFolders"
+}
+
+getCountFiles() {
+  local countFiles=$(($RANDOM%5)) 
+  countFiles=$(($countFiles+1))
+  echo "$countFiles"
+}
+
+getRandomDirectory() {
+  local dir="$1"
+  local max_depth=10  # Защита от бесконечной рекурсии
+  local current_depth=${2:-0}
+
+  # Защита от слишком глубокой рекурсии
+  if (( current_depth > max_depth )); then
+    echo "$dir"
+    return
+  fi
+
+  # Получаем список поддиректорий безопасным способом
+  local subdirs=()
+  while IFS= read -r -d $'\0' subdir; do
+    subdirs+=("$subdir")
+  done < <(find "$dir" -mindepth 1 -maxdepth 1 -type d -print0)
+
+  local countDir=${#subdirs[@]}
+
+  # Если нет поддиректорий, возвращаем текущую
+  if (( countDir == 0 )); then
+    echo "$dir"
+    return
+  fi
+
+  # Выбираем случайную поддиректорию
+  local random_index=$(( RANDOM % countDir ))
+  local selected_dir="${subdirs[random_index]}"
+
+  # С вероятностью 50% идём глубже
+  if (( RANDOM % 2 == 0 )); then
+    getRandomDirectory "$selected_dir" $(( current_depth + 1 ))
+  else
+    echo "$selected_dir"
+  fi
+}
