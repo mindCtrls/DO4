81a46d37840c675c13a32e5e79a8f5bbc72f5eaf
Fluid Oxygen <tosha.iljin55@gmail.com>
2025-06-16 17:00:01 +0700

    4 - синтаксис везде один сделать, убрать комментарии, мб в логе убрать перенос на последней строке

--- src/04/main.sh
+++ src/04/main.sh
@@ -24,39 +24,7 @@ getStatusCode() {
 getMethode() {
   local methode=${methodeArr[$(getRandom "4")]}
 
-  echo "$methodeArr"
-}
-
-
-getDate() {
-  local day=$((19 + $1))
-  local mon=Feb
-  local year=2025
-  local timeZone=+0700
-
-  local hour=01
-  local min=00
-  local sec=00
-
-  local newValue=$(getRandom "60")
-  sec=$((sec + newValue))
-
-  if ((sec >= 60)); then
-    min=$((min + sec / 60))
-    sec=$((sec % 60))
-  fi
-
-  if ((min >= 60)); then
-    hour=$((hour + min / 60))
-    min=$((min % 60))
-  fi
-
-  printf -v day "%02d" "$day"
-  printf -v hour "%02d" "$hour"
-  printf -v min "%02d" "$min"
-  printf -v sec "%02d" "$sec"
-
-  echo "$day/$mon/$year:$hour:$min:$sec $timeZone"
+  echo "$methode"
 }
 
 getUrl() {
@@ -71,11 +39,41 @@ getAgent() {
   echo "$agent"
 }
 
+getDate() {
+  local day=$((19 + $1))
+  local mon=Feb
+  local year=2025
+  local timeZone=+0700
+
+  local hour=0
+  local min=0
+  local sec=0
+
+  local newValue=$2
+  ((sec += newValue))
+
+  if ((sec >= 60)); then
+    ((min += sec / 60))
+    ((sec %= 60))
+  fi
+  if ((min >= 60)); then
+    ((hour += min / 60))
+    ((min %= 60))
+  fi
+
+  printf -v day "%02d" "$day"
+  printf -v hour "%02d" "$hour"
+  printf -v min "%02d" "$min"
+  printf -v sec "%02d" "$sec"
+
+  echo "$day/$mon/$year:$hour:$min:$sec $timeZone"
+}
+
 oneLogCreate() {
   local ip=$(getIp)
   local statusCode=$(getStatusCode)
   local methode=$(getMethode)
-  local date=$(getDate $1)
+  local date=$(getDate "$1" "$2")
   local url=$(getUrl)
   local agent=$(getAgent)
 
@@ -83,13 +81,18 @@ oneLogCreate() {
 }
 
 makeLogs() {
-local countRecord=$(getRandom "100" "1000")
+  local countIterates=$(getRandom "100" "1000")
 
   for ((i=0; i<5; i++)) {
-    for ((j=0; j<"$countRecord"; j++)) {
-      recordToLog $i "$(oneLogCreate "$i")"
+    local secIterator=$(getRandom "60")
+    
+    for ((j=0; j<"$countIterates"; j++)) {
+      recordToLog "$i" "$(oneLogCreate "$i" "$secIterator")"
+
+      local newValue=$(getRandom "60")
+      ((secIterator += newValue))
     }
   }
 }
 
-$(makeLogs)
+makeLogs
--- src/04/recordModule.sh
+++ src/04/recordModule.sh
@@ -1,8 +1,8 @@
 recordToLog() {
   local recordFileName="record_$1.log"
-  
-  read -r ip statusCode method date url agent <<< "$2"
+  local byteSize=1234
+  read -r ip statusCode method date timeZone url agent <<< "$2"
     
-  printf "%-18s | %-5s | %-8s | %-19s | %-40s | %-30s\n" \
-      "$ip" "$statusCode" "$method" "$date" "$url" "$agent" >> "$recordFileName"
+  printf "%s - - [%s %s] \"%s %s HTTP/1.1\" %s %s %s\n" \
+      "$ip" "$date" "$timeZone" "$method" "$url" "$statusCode" "$byteSize" "$agent" >> "$recordFileName"
 }
